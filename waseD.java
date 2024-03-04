eef21d39a3172e01e0d5fd39be608e7996b18d70
Arjyan <arjyanbibhushan251@gmail.com>
2024-03-04 23:24:40 +0530

    Third commit

---
+++ waseD.java
@@ -0,0 +1,27 @@
+public class waseD {
+    public static void main(String[] args)
+
+ {
+
+    if(x>=10);
+
+   {        if(x>=20)
+
+             System.out.println(" 1");
+
+        else
+
+             System.out.println(" 2");
+
+   }     
+
+     else
+
+      System.out.println(" 3"); 
+
+    }
+
+ System.out.println(" 4");
+
+}
+
